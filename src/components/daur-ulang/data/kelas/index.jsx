3232de008771ca6ef33a8c708548c3cfd0e6d8c7
aameliazn <aameliazulkarnaen@gmail.com>
2023-11-22 12:47:03 +0700

    update daur ulang data kelas

--- src/components/daur-ulang/data/kelas/index.jsx
+++ src/components/daur-ulang/data/kelas/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import FetchKelas from './fetchKelas'
+import React from "react";
+import FetchKelas from "./fetchKelas";
+import Breadcrumb from "@/components/layouts/breadcrumb";
+import { HomeFilled } from "@ant-design/icons";
 
 export default function index() {
   return (
     <>
-    <FetchKelas/>
+      <Breadcrumb
+        breadcrumbs={[
+          { label: <HomeFilled />, url: "/" },
+          { label: "Daur Ulang", url: "/daur-ulang" },
+          { label: "Data Kelas", url: "/daur-ulang/data/kelas/{id}" },
+        ]}
+      />
+      <FetchKelas />
     </>
-  )
+  );
 }
