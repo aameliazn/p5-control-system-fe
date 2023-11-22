2af1dc9c6e784b6df600d8904cfcbd33abcb53f7
aameliazn <aameliazulkarnaen@gmail.com>
2023-11-22 18:54:02 +0700

    update portofolio data kelas

--- src/app/portofolio/data/kelas/[id]/page.jsx
+++ src/app/portofolio/data/kelas/[id]/page.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Layout from '@/components/layouts'
-import Index from '@/components/portofolio/data/kelas'
+import React from "react";
+import Layout from "@/components/layouts";
+import DataKelas from "@/components/portofolio/data/kelas";
+import PortofolioProvider from "@/context/PortofolioContext";
+
 export default function page() {
   return (
     <>
-    <Layout>
-        <Index/>
-    </Layout>
+      <Layout>
+        <PortofolioProvider>
+          <DataKelas />
+        </PortofolioProvider>
+      </Layout>
     </>
-  )
+  );
 }
