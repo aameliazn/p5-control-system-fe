1fdfb9b86d88e4281103d5c429e8a5b47aecf487
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-04 15:37:36 +0700

    update kebersihan sekolah siswa page

--- src/app/kebersihan-sekolah/data/siswa/[id]/page.jsx
+++ src/app/kebersihan-sekolah/data/siswa/[id]/page.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Index from '@/components/kebersihan-sekolah/data/siswa'
-import Layout from '@/components/layouts'
+import React from "react";
+import Layout from "@/components/layouts";
+import DataSiswa from "@/components/kebersihan-sekolah/data/siswa";
+import KebersihanSekolahProvider from "@/context/KebersihanSekolahContext";
 
 export default function page() {
   return (
-   <>
-   <Layout>
-    <Index/>
-   </Layout>
-   </>
-  )
+    <>
+      <Layout>
+        <KebersihanSekolahProvider>
+          <DataSiswa />
+        </KebersihanSekolahProvider>
+      </Layout>
+    </>
+  );
 }
