856f0151af2dec5678412a3bbbc97e6d3f82a527
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-04 15:44:10 +0700

    update tanaman siswa page

--- src/components/tanaman/data/siswa/index.jsx
+++ src/components/tanaman/data/siswa/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import FetchTable from './fetchTable'
+import React from "react";
+import FetchTable from "./fetchTable";
+import Breadcrumb from "@/components/layouts/breadcrumb";
+import { HomeFilled } from "@ant-design/icons";
 
 export default function index() {
   return (
     <div>
-  <FetchTable/>
+      <Breadcrumb
+        breadcrumbs={[
+          { label: <HomeFilled />, url: "/" },
+          { label: "Tanaman", url: "/tanaman" },
+          { label: "Data Kelas", url: "/tanaman/data/kelas/[id]?" },
+          { label: "Data Siswa", url: "/tanaman/data/siswa/[id]?" },
+        ]}
+      />
+      <FetchTable />
     </div>
-  
-  )
+  );
 }
