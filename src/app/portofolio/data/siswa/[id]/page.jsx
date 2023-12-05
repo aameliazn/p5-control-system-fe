e5362732fb7511989b6b84e54ee846318b850b3b
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-05 08:04:51 +0700

    update portofolio siswa page

--- src/app/portofolio/data/siswa/[id]/page.jsx
+++ src/app/portofolio/data/siswa/[id]/page.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Layout from '@/components/layouts'
-import Index from '@/components/portofolio/data/siswa'
+import React from "react";
+import Layout from "@/components/layouts";
+import DataSiswa from "@/components/portofolio/data/siswa";
+import PortofolioProvider from "@/context/PortofolioContext";
 
 export default function page() {
   return (
     <>
-    <Layout>
-        <Index/>
-    </Layout>
+      <Layout>
+        <PortofolioProvider>
+          <DataSiswa />
+        </PortofolioProvider>
+      </Layout>
     </>
-  )
+  );
 }
