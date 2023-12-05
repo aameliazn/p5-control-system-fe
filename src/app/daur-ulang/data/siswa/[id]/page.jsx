3ac784d172ffa269f899b1142969e63ffe305a79
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-05 08:09:04 +0700

    add breadcrumbs daur ulang siswa page

--- src/app/daur-ulang/data/siswa/[id]/page.jsx
+++ src/app/daur-ulang/data/siswa/[id]/page.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Layout from '@/components/layouts'
-import Index from '@/components/daur-ulang/data/siswa'
+import React from "react";
+import Layout from "@/components/layouts";
+import DaurUlang from "@/components/daur-ulang/data/siswa";
+import DaurUlangProvider from "@/context/DaurUlangContext";
 
 export default function page() {
   return (
     <>
-    <Layout>
-        <Index/>
-    </Layout>
+      <Layout>
+        <DaurUlangProvider>
+          <DaurUlang />
+        </DaurUlangProvider>
+      </Layout>
     </>
-  )
+  );
 }
