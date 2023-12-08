0d47ce723fabadbaf2e3fde34113dc679ff45ce2
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-08 08:54:31 +0700

    update materi role superadmin, admin

--- src/components/materi/index.jsx
+++ src/components/materi/index.jsx
@@ -1,33 +1,54 @@
 "use client";
 import React from "react";
-import Materi from "./materiClass";
-import DataClass from "./dataClass";
-import InputMateri from "./inputMateri";
+import { Space } from "antd";
+import Chart from "./admin/chart";
+import Header from "./admin/header";
+import Materi from "./super-admin/materiClass";
 import Breadcrumb from "../layouts/breadcrumb";
 import { HomeFilled } from "@ant-design/icons";
+import DataClass from "./super-admin/dataClass";
 import { useMateri } from "@/context/MateriContext";
-import {Space} from 'antd';
+import InputMateri from "./super-admin/inputMateri";
 
 export default function index() {
   const { visible } = useMateri();
-  
+
+  const dataUser = JSON.parse(localStorage.getItem("user"));
+
   return (
     <>
-        <Space
-    direction="vertical"
-    style={{
-      width: '100%',
-    }}
-  ></Space>
+      <Space
+        direction="vertical"
+        style={{
+          width: "100%",
+        }}
+      ></Space>
       <Breadcrumb
         breadcrumbs={[
           { label: <HomeFilled />, url: "/" },
           { label: "Materi", url: "/materi" },
         ]}
       />
+
       {visible && <InputMateri />}
-      <Materi />
-      <DataClass />
+
+      {dataUser?.role == "superadmin" && (
+        <div>
+          <Materi />
+          <DataClass />
+        </div>
+      )}
+
+      {dataUser?.role == "admin" && (
+        <div>
+          <div>
+            <Header />
+          </div>
+          <div className="mt-4">
+            <Chart />
+          </div>
+        </div>
+      )}
     </>
   );
 }
