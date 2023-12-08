eaeb7e73fdd1f67a1884442f7033a47f2bf6a25c
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-08 10:27:13 +0700

    update daur ulang role superadmin, admin, user

--- src/app/daur-ulang/data/kelas/[id]/page.jsx
+++ src/app/daur-ulang/data/kelas/[id]/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@/components/layouts";
-import DataKelas from "@/components/daur-ulang/data/kelas";
+import DataKelas from "@/components/daur-ulang/super-admin/data/kelas";
 import DaurUlangProvider from "@/context/DaurUlangContext";
 
 export default function page() {
--- src/components/daur-ulang/index.jsx
+++ src/components/daur-ulang/index.jsx
@@ -1,14 +1,20 @@
 "use client";
 import React from "react";
-import DataSiswa from "./dataSiswa";
-import InputRuangan from "./inputRuangan";
-import TableRuangan from "./tableRuangan";
+import AdminChart from "./admin/chart";
+import AdminTable from "./admin/table";
+import TableUser from "./user/table";
+import KegiatanTable from "./user/kegiatanTable";
+import DataSiswa from "./super-admin/dataSiswa";
+import InputRuangan from "./super-admin/inputRuangan";
+import TableRuangan from "./super-admin/tableRuangan";
 import Breadcrumb from "../layouts/breadcrumb";
 import { HomeFilled } from "@ant-design/icons";
 import { useDaurUlang } from "@/context/DaurUlangContext";
 
 export default function index() {
   const { visible } = useDaurUlang();
+
+  const dataUser = JSON.parse(localStorage.getItem("user"));
 
   return (
     <>
@@ -18,9 +24,37 @@ export default function index() {
           { label: "Daur Ulang", url: "/daur-ulang" },
         ]}
       />
+
       {visible && <InputRuangan />}
-      <TableRuangan />
-      <DataSiswa />
+
+      {dataUser?.role == "superadmin" && (
+        <div>
+          <TableRuangan />
+          <DataSiswa />
+        </div>
+      )}
+
+      {dataUser?.role == "admin" && (
+        <div>
+          <div className="mt-1">
+            <AdminChart />
+          </div>
+          <div className="mt-1">
+            <AdminTable />
+          </div>
+        </div>
+      )}
+
+      {dataUser?.role == "user" && (
+        <div>
+          <div>
+            <KegiatanTable />
+          </div>
+          <div className="mt-2">
+            <TableUser />
+          </div>
+        </div>
+      )}
     </>
   );
 }
