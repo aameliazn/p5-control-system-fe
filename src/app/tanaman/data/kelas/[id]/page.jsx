deef497b905706c3ad3ce9270d26effb7c4ceda8
aameliazn <aameliazulkarnaen@gmail.com>
2023-12-08 09:54:00 +0700

    update tanaman superadmin, user

--- src/app/tanaman/data/kelas/[id]/page.jsx
+++ src/app/tanaman/data/kelas/[id]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "@/components/layouts";
 import TanamanProvider from "@/context/TanamanContext";
-import DataKelasTanaman from "@/components/tanaman/data/kelas";
+import DataKelasTanaman from "@/components/tanaman/super-admin/data/kelas";
 
 export default function page() {
   return (
