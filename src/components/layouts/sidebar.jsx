3fc132f1aebf1402951128e6e4789ee4b5f62750
GiffaIndr <giffasyaikhani2005@gmail.com>
2024-01-15 13:47:24 +0700

    icons sidebar

--- src/components/layouts/sidebar.jsx
+++ src/components/layouts/sidebar.jsx
@@ -8,6 +8,7 @@ import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import { usePathname, useRouter } from "next/navigation";
 import { MdGrass, MdOutlineRecycling } from "react-icons/md";
 import { GiObservatory } from "react-icons/gi";
+import { TbMessage2Question } from "react-icons/tb";
 import Logo from "../../../public/logo/logo_wikrama.png";
 
 export default function sidebar({ children }) {
@@ -236,20 +237,7 @@ export default function sidebar({ children }) {
                           pathname == "/portofolio" ? "menu-active" : ""
                         }`}
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5 opacity-75"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                          />
-                        </svg>
+                       <TbMessage2Question className="h-5 w-5 opacity-75"/>
                         <span>Portofolio</span>
                       </li>
                     </Link>
